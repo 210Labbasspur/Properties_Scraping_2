440d800ab7630c12b84ecc0db2b7d8499332b3ed
Syed Hassan Mujtaba Sherazi <syedhassanmujtabasherazi@gmail.com>
2026-02-02 11:15:47 +0500

    Cooke_Rent - https://cookeproperty.com.au/
    Scrapy script to extract data and save into MongoDB, while following a data-format. Source = https://cookeproperty.com.au/

---
+++ Cooke_Rent/Cooke_Rent/spiders/Cooke_Rent.py
@@ -0,0 +1,381 @@
+##############33            Cooke_Rent
+
+import random
+import mimetypes
+from botocore.exceptions import NoCredentialsError, ClientError
+import requests, json, time, threading, queue, os
+import boto3
+import pymongo
+from datetime import datetime
+from botocore.exceptions import NoCredentialsError
+import scrapy
+
+
+class Cooke_Rent(scrapy.Spider):
+    name = "Cooke_Rent"
+    url = ('https://cookeproperty.com.au/wp-admin/admin-ajax.php?action=get_posts&query%5Bpost_type%5D%5Bvalue%5D=listings&query%5Bcount%5D%5Bvalue%5D=20&query%5Borderby%5D%5Bvalue%5D=meta_value&query%5Bmeta_key%5D%5Bvalue%5D=dateListed&query%5Bsold%5D%5Bvalue%5D=0&query%5BsaleOrRental%5D%5Bvalue%5D=Rental&query%5BsaleOrRental%5D%5Btype%5D=equal&query%5Border%5D%5Bvalue%5D=dateListed-desc&query%5Border%5D%5Btype%5D=equal&query'
+           '%5Bpaged%5D%5Bvalue%5D={}')
+    prefix = "https://www.cookeproperty.com.au"
+    headers = {
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7',
+        'priority': 'u=0, i',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    }
+
+    count = 1
+    db = 'Cooke'
+    collection = 'Cooke_Rent'
+    bucket_prefix = f'Images6'
+
+    local_file_path = '/img'
+
+    def __init__(self, *args, **kwargs):
+        super(Cooke_Rent, self).__init__(*args, **kwargs)
+        self.links = []
+        self.database_sale_matching_url = self.read_data_base()
+
+
+    '''    SCRPAY SCRIPT START'S FROM HERE     '''
+    def start_requests(self):
+        page_no = 1
+        yield scrapy.Request(url=self.url.format(page_no), callback=self.parse, headers=self.headers, meta={'page_no':page_no})
+
+
+    def parse(self, response):
+        '''    FARHAN'S LOGIC    '''
+        data = json.loads(response.text)
+        links = []
+        for each_url in data['data']['listings']:
+            each_url = each_url.get('url')
+            links.append(each_url)
+            self.links.append(each_url)
+
+        for each_db_detail_page_url in self.database_sale_matching_url:
+            if each_db_detail_page_url in links:
+                new_data = {
+                    "Field102": "ACTIVE",
+                    "Field104": each_db_detail_page_url,
+                    "Field3": datetime.now().strftime("%d %b, %Y")
+                }
+                self.update_database(each_db_detail_page_url, new_data, 'true')
+                index_to_remove = links.index(each_db_detail_page_url)
+                links.pop(index_to_remove)
+
+            elif each_db_detail_page_url not in self.links:
+                new_data = {
+                    "Field102": "REMOVED",
+                    "Field104": each_db_detail_page_url,
+                    "Field3": datetime.now().strftime("%d %b, %Y")
+                }
+                self.update_database(each_db_detail_page_url, new_data, 'false')
+
+
+        for property in data['data']['listings']:
+            property_url = property.get('url')
+            print(self.count,' # Property URL :', property_url)
+            self.count += 1
+            yield response.follow(property_url, headers=self.headers, callback=self.detail_parse, meta={'property':property})
+
+        total_pages = data['data']['max_num_pages']
+        page_no = response.meta['page_no']
+        print(f"Current-Page = {page_no} || Total-Pages = {total_pages}")
+        if page_no < total_pages:
+            page_no += 1
+            yield scrapy.Request(url=self.url.format(page_no), callback=self.parse, headers=self.headers, meta={'page_no':page_no})
+
+
+    def detail_parse(self, response):
+        property = response.meta['property']
+        detail_page_url = response.url
+        database_sale_matching_url = self.read_data_base()
+        if detail_page_url not in database_sale_matching_url:
+            item = dict()
+            item['Field1'] = ''
+            item['Field2'] = int(2014)
+            item['Field3'] = datetime.now().date().strftime('%Y-%m-%d')  # date_data_scanned
+            item['Field4'] = 'Cooke Property Agents'
+
+            item['Field5'] = property.get('displayAddress','')
+
+            item['Field6'] = property.get('detailsBeds','')
+            item['Field7'] = property.get('detailsBaths','')
+            item['Field8'] = property.get('detailsCarAccom','')
+
+            item['Field9'] = response.css('.listing-info-price ::text').get('').strip().replace('$','').replace(',','').replace(' ','').replace('ContactAgent','')
+            item['Field12'] = ' '.join(e.strip() for e in response.css('.post-content ::text').getall())
+
+            '''         Uploading Images on Wasabi S3 Bucket            '''
+            prop_images = response.css("#media-gallery a.listing-media-slideimg ::attr(href)").getall()
+            images = []
+            for img in prop_images:
+                img = img.replace('background-image:url(','').replace(");","")
+                images.append(f'https:{img}' if img else "")
+            images_string = ', '.join(images)
+            # item['Field13'] = images_string
+            # print('Property images are :', images_string)
+            random_number = random.randint(1, 10000000000)
+            item['Field13'] = ", ".join(self.uploaded(images_string, random_number))
+
+            item['Field14'] = response.url  # external_link
+            '''          AGENTS          '''
+            if response.css(".col-md-right .property-agent-details-wrapper .agent-mb"):
+                agent1 = response.css(".col-md-right .property-agent-details-wrapper .agent-mb")[0]
+                agent1_name = agent1.css('.staff-card-title a ::text').get('').strip()
+                if agent1_name:
+                    first_name1, last_name1 = agent1_name.split(maxsplit=1)
+                    item['Field15'] = first_name1.strip()  # agent_first_name_1
+                    item['Field16'] = last_name1.strip()  # agent_surname_name_1
+                    item['Field17'] = agent1_name.strip()  # agent_full_name_1
+                    item['Field18'] = agent1.css('.staff-role ::text').get('').strip()
+                    item['Field19'] = agent1.xpath(".//a[contains(@href,'mailto:')]/@href").get('').strip().replace('mailto:','')
+                    item['Field20'] = agent1.xpath(".//*[contains(@data-phonelabel,'agent_mobile')]/a/text()").get('').strip().replace('mailto:','')
+                    item['Field21'] = agent1.xpath(".//*[contains(@data-phonelabel,'phone')]/a/text()").get('').strip().replace('mailto:','')
+                    '''         Uploading Images on Wasabi S3 Bucket            '''
+                    agent_image = agent1.css(".agent-mb-image-border ::attr(style)").get('').strip().replace('background-image:url(','').replace(')','')
+                    images_string = f'{agent_image}' if agent_image else ""
+                    # item['Field22'] = images_string
+                    # print('Agent1 image is :',images_string)
+                    random_number = random.randint(1, 10000000000)
+                    item['Field22'] = ",".join(self.uploaded(images_string, random_number))
+
+            if len(response.css(".col-md-right .property-agent-details-wrapper .agent-mb").getall()) > 1:
+                agent2 = response.css(".col-md-right .property-agent-details-wrapper .agent-mb")[1]
+                agent2_name = agent2.css('.staff-card-title a ::text').get('').strip()
+                if agent2:
+                    item['Field23'] = agent2_name.strip()
+                    item['Field24'] = agent2.css('.staff-role ::text').get('').strip()
+                    item['Field25'] = agent2.xpath(".//a[contains(@href,'mailto:')]/@href").get('').strip().replace('mailto:','')
+                    item['Field26'] = agent2.xpath(".//*[contains(@data-phonelabel,'agent_mobile')]/a/text()").get('').strip().replace('mailto:','')
+                    item['Field26A'] = agent2.xpath(".//*[contains(@data-phonelabel,'phone')]/a/text()").get('').strip().replace('mailto:','')
+                    '''         Uploading Images on Wasabi S3 Bucket            '''
+                    agent_image = agent2.css(".agent-mb-image-border ::attr(style)").get('').strip().replace('background-image:url(','').replace(')','')
+                    images_string = f'{agent_image}' if agent_image else ""
+                    # item['Field27'] = images_string
+                    # print('Agent2 image is :',images_string)
+                    random_number = random.randint(1, 10000000000)
+                    item['Field27'] = ",".join(self.uploaded(images_string, random_number))
+
+            if len(response.css(".col-md-right .property-agent-details-wrapper .agent-mb").getall()) > 2:
+                agent3 = response.css(".col-md-right .property-agent-details-wrapper .agent-mb")[2]
+                agent3_name = agent3.css('.staff-card-title a ::text').get('').strip()
+                if agent3:
+                    item['Field28'] = agent3_name.strip()
+                    item['Field29'] = agent3.css('.staff-role ::text').get('').strip()
+                    item['Field30'] = agent3.xpath(".//a[contains(@href,'mailto:')]/@href").get('').strip().replace('mailto:','')
+                    item['Field31'] = agent3.xpath(".//*[contains(@data-phonelabel,'agent_mobile')]/a/text()").get('').strip().replace('mailto:','')
+                    item['Field31A'] = agent3.xpath(".//*[contains(@data-phonelabel,'phone')]/a/text()").get('').strip().replace('mailto:','')
+                    '''         Uploading Images on Wasabi S3 Bucket            '''
+                    agent_image = agent3.css(".agent-mb-image-border ::attr(style)").get('').strip().replace('background-image:url(','').replace(')','')
+                    images_string = f'{agent_image}' if agent_image else ""
+                    # item['Field32'] = images_string
+                    # print('Agent3 image is :',images_string)
+                    random_number = random.randint(1, 10000000000)
+                    item['Field32'] = ", ".join(self.uploaded(images_string, random_number))
+
+
+            property_id = response.xpath("//*[contains(text(),'property ID')]/parent::div[1]/text()").getall()
+            item['Field33'] = property_id[-1].strip().replace('\n','') if property_id else ''
+            # item['Field33'] = property.get('id')
+            item['Field35'] = property.get('type')
+
+            item['Field36'] = ''
+            if property.get('landAreaUnit') == 'ac':
+                land_area_sqm = float(property.get('landArea')) * 4046.85642
+                item['Field36'] = land_area_sqm
+            elif property.get('landAreaUnit') == 'm2':
+                item['Field36'] = property.get('landArea')
+
+            if property.get('buildAreaUnit') == 'ac':
+                land_area_sqm = float(property.get('buildArea')) * 4046.85642
+                item['Field37'] = land_area_sqm
+            elif property.get('buildAreaUnit') == 'm2':
+                item['Field37'] = property.get('buildArea')
+
+            feature_count = 58
+            for feature in response.css(".col-md-left .property-feature-list li"):   ##  first 4 features are ID, bed, bath etc.
+                if 'condition' in feature.css('::text').get('').strip().lower():
+                    item['Field52'] = feature.css('::text').get('').strip()
+                else:
+                    item[f'Field{feature_count}'] = feature.css('::text').get('').strip()
+                    feature_count += 1
+
+
+            ####        ACTIVE / REMOVED Logic implemented in "Field102"
+            item['Field102'] = 'ACTIVE'
+            item['Field104'] = response.url
+            print(item)
+            self.insert_database(item)
+        else:
+            print(f'Skipping Record {detail_page_url}, since it already exists in the Data Base  !!!!! ')
+
+
+    def read_data_base(self):
+    # def read_data_base(self, profileUrl):
+        connection_string = 'mongodb://localhost:27017'
+        conn = pymongo.MongoClient(connection_string)
+        db = conn[self.db]
+        collection = db[self.collection]
+
+        sale_urls_list_of_DB = []
+        all_matching_data = collection.find()
+        for each_row in all_matching_data:
+            # sale = each_row.get('Field104')
+            sale = each_row.get('Field14')
+            sale_urls_list_of_DB.append(sale)
+        return sale_urls_list_of_DB
+
+
+    def insert_database(self, new_data):
+        connection_string = 'mongodb://localhost:27017'
+        conn = pymongo.MongoClient(connection_string)
+        db = conn[self.db]
+        collection = db[self.collection]
+        collection.insert_one(new_data)
+        print("Data inserted successfully!")
+
+    def update_database(self, profileUrl, new_data, area):
+        connection_string = 'mongodb://localhost:27017'
+        conn = pymongo.MongoClient(connection_string)
+        db = conn[self.db]
+        collection = db[self.collection]
+
+        # search_query = {"Field104": profileUrl}
+        search_query = {"Field14": profileUrl}
+
+        update_query = {
+            "$set": {
+                "Field3": new_data["Field3"],
+                "Field102": new_data["Field102"],
+                "Field104": new_data["Field104"]
+            }
+        }
+        collection.update_one(search_query, update_query, upsert=True)
+        print(f"Data updated successfully! + {area} : ",search_query)
+
+
+    '''    HANDLING IMAGES - WASABI     '''
+    def download_image(self, img_url, file_path, name):
+        try:
+            response = requests.request(method='GET', url=img_url)
+            if response.status_code == 200:
+                # sanitized_name = self.sanitize_filename(name)
+                with open(f'{file_path}/{name}', 'wb') as file:
+                    file.write(response.content)
+                print(f"{name} Image downloaded successfully.")
+            else:
+                print("Failed to download the image. Status code:", response.status_code)
+        except requests.exceptions.RequestException as e:
+            print("An error occurred while downloading:", e)
+        except Exception as e:
+            print(e)
+
+    def create_new_bucket(self, bucket_prefix, bucket_number, s3):
+        new_bucket_name = f'{bucket_prefix}_{bucket_number}'
+        s3.create_bucket(Bucket=new_bucket_name)
+        return new_bucket_name
+
+    def uploaded(self, list_of_img, names):
+        list_images = [url.strip() for url in list_of_img.split(',')]
+        wasabi_access_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        wasabi_secret_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        s3 = boto3.client(
+            's3',
+            aws_access_key_id=wasabi_access_key,
+            aws_secret_access_key=wasabi_secret_key,
+            endpoint_url="https://s3.ap-southeast-1.wasabisys.com",
+        )
+        # bucket_prefix = 'BLS'
+        bucket_prefix = self.bucket_prefix
+        bucket_number = 1
+        current_bucket_name = f'{bucket_prefix}_{bucket_number}'
+
+        # existing_buckets = [bucket['Name'] for bucket in s3.list_buckets()['Buckets']]
+        existing_buckets = [bucket['Name'] for bucket in s3.list_buckets()['Buckets']]
+        if current_bucket_name in existing_buckets:
+            if current_bucket_name != self.create_new_bucket(bucket_prefix, bucket_number, s3):
+                print("Bucket already exists. Create Buket and Run...")
+                return
+
+        current_bucket_name = self.create_new_bucket(bucket_prefix, bucket_number, s3)
+        print(f"using bucket: {current_bucket_name}")
+        try:
+            object_count = len(s3.list_objects(Bucket=current_bucket_name).get('Contents', []))
+        except s3.exceptions.NoSuchBucket:
+            current_bucket_name = self.create_new_bucket(bucket_prefix, bucket_number, s3)
+            object_count = 0
+        wasabi_url = []
+
+        # create image folder automacity
+        current_directory = os.getcwd()
+        image_folder = 'images'
+        image_directory = os.path.join(current_directory, image_folder)
+        os.makedirs(image_directory, exist_ok=True)
+
+        for index, img in enumerate(list_images, start=1):
+            try:
+                image_url = img
+                # local_file_path = 'C:/Users/Jahanzaib/Desktop/img/'
+                local_file_path = os.path.join(image_directory).replace('\\', '/')
+
+                title_name = f'{names}_{index}.jpg'
+
+                img_url = f'https://s3.ap-southeast-1.wasabisys.com/{current_bucket_name}/{title_name}'
+                if image_url:
+                    self.download_image(image_url, local_file_path, title_name)
+                    # download_image(image_url,a)
+
+                if object_count >= 10000000:
+                    bucket_number += 1
+                    current_bucket_name = self.create_new_bucket(bucket_prefix, bucket_number, s3)
+                    object_count = 0
+
+                bucket_policy = {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Principal": "*",
+                            "Action": "s3:GetObject",
+                            "Resource": f"arn:aws:s3:::{current_bucket_name}/*"
+                        }
+                    ]
+                }
+
+                s3.put_bucket_policy(Bucket=current_bucket_name, Policy=json.dumps(bucket_policy))
+                # uploading
+                file_path_on_disk = os.path.join(local_file_path, title_name)
+
+                try:
+                    content_type, _ = mimetypes.guess_type(file_path_on_disk)
+                    s3.upload_file(
+                        file_path_on_disk,
+                        current_bucket_name,
+                        title_name,
+                        ExtraArgs={'ContentType': content_type} if content_type else None
+                    )
+                    self.delete_local_image(file_path_on_disk)
+                except NoCredentialsError:
+                    print("Credentials not available")
+                except ClientError as e:
+                    print(f"An error occurred: {e}")
+                object_count += 1
+                wasabi_url.append(img_url)
+            except Exception as e:
+                print(e)
+        return wasabi_url
+
+    def delete_local_image(self, file_path):
+        try:
+            os.remove(file_path)
+        except OSError as e:
+            print(f"Error deleting local image: {e}")
+
+
